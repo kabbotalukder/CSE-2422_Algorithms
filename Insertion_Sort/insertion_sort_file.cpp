e201b6124aa216b39f9ee62d5b86f5135a6bbd27
Tamiz Ullah Kabbo <kabbotalukder5@gmail.com>
2025-02-25 12:05:13 +0600

    500k added

--- Insertion_Sort/insertion_sort_file.cpp
+++ Insertion_Sort/insertion_sort_file.cpp
@@ -12,10 +12,13 @@ void printarr(const vector<int>& a) {
     cout << endl;
 }
 
-void insertion_sort(vector<int>& a) {
-    for (size_t i = 1; i < a.size(); ++i) {
-        int key = a[i];
-        int j = i - 1;
+void insertion_sort(vector<long long>& a) {
+    long long count = 0;
+    for (long long i = 1; i < a.size(); ++i) {
+        count++;
+        cout << count << " sorted" << endl;
+        long long key = a[i];
+        long long j = i - 1;
         while (j >= 0 && a[j] > key) {
             a[j + 1] = a[j];
             j--;
@@ -27,26 +30,26 @@ void insertion_sort(vector<int>& a) {
 int main() {
     clock_t start, finish;
     double est;
-    ifstream inputFile("inputs_100K.txt");
+    ifstream inputFile("inputs_500K.txt");
     if (!inputFile.is_open()) {
         cerr << "Error opening file 'inputs.txt'" << endl;
         return 1;
     }
 
-    vector<int> a;
-    int value;
+    vector<long long> a;
+    long long value;
     while (inputFile >> value) {
         a.push_back(value);
     }
     inputFile.close();
-    cout << "Array before sorting: " << endl;
-    printarr(a);
+    // cout << "Array before sorting: " << endl;
+    // printarr(a);
     start = clock();
     insertion_sort(a);
     finish = clock();
     est = (double)((finish - start) / CLOCKS_PER_SEC);
-    cout << "Array after sorting: " << endl;
-    printarr(a);
+    // cout << "Array after sorting: " << endl;
+    // printarr(a);
     cout << "Input Size: " << a.size() << endl;
     cout << "Time taken: " << est << endl;
     return 0;
--- Insertion_Sort/random_numbers.py
+++ Insertion_Sort/random_numbers.py
@@ -1,10 +1,10 @@
 import random
 
-def generate_random_numbers(filename, count, min_val=1, max_val=10000000):
+def generate_random_numbers(filename, count, min_val=1, max_val=500000):
     with open(filename, 'w') as f:
         for _ in range(count):
             f.write(f"{random.randint(min_val, max_val)}\n")
 
-if __name__ == "__main__":
-    generate_random_numbers("inputs_10M.txt", 10000000)
-    print("Generated 100000 random numbers and saved them to inputs.txt")
+
+generate_random_numbers("inputs_500K.txt", 500000)
+print("Generated random numbers and saved them to inputs.txt")
